667f48fa34b3c3614b2d054fb781cba04641c176
Matheus Ramos dos Santos <mattheusdossaantos@gmail.com>
2024-02-25 15:05:32 -0300

    Router configurations

--- src/router.jsx
+++ src/router.jsx
@@ -3,15 +3,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import DonationSection from "./pages/DonationSection";
 import Home from "./pages/Home";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/karate-landing-page",
-    element: <Home />,
-  },
-  {
-    path: "/DonationSection",
-    element: <DonationSection />,
+    element: <App />,
+    children: [
+      {
+        path: "/karate-landing-page",
+        element: <Home />,
+      },
+      {
+        path: "/karate-landing-page/DonationSection",
+        element: <DonationSection />,
+      },
+    ],
   },
 ]);
 
